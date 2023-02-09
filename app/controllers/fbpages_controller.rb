79bcfe888618b5fef5037580675574cb56d15d98
jasper <jasper@bootyard.com>
2023-02-09 11:43:25 +0800

    initial commit for user and devise

---
+++ app/controllers/fbpages_controller.rb
@@ -0,0 +1,6 @@
+class FbpagesController < ApplicationController
+    before_action :authenticate_user!
+    def index
+       
+    end
+end
---
+++ test/controllers/fbpages_controller_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class FbpagesControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
